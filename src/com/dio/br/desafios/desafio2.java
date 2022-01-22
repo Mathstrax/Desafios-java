ee862ed664b423609a2fd5ea5d48e3d20d8fcfbb
Matheus De Alencar <mathstrax@gmail.com>
2022-01-22 19:15:45 -0300

    commitando desafios da dio

---
+++ src/com/dio/br/desafios/desafio2.java
@@ -0,0 +1,21 @@
+package com.dio.br.desafios;
+
+import java.util.Scanner;
+
+public class desafio2 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int nota;
+
+            while (true) { // estrutura de repetição irá perguntar as notas
+                System.out.printf("Digite sua nota de 0 à 10: ");
+                nota = ler.nextInt();// guarda o valor da nota
+                if (nota > 10) { // se o usuario digitar um valor acima de 10 vai entrar no looping caso contrario o programa ira encerrar
+                    System.out.printf("Nota invalida, por favor, ");
+                } else {
+                    break;
+                }
+
+        }
+    }
+}
---
+++ src/com/dio/br/desafios/desafios.java
@@ -0,0 +1,26 @@
+package com.dio.br.desafios;
+
+import java.util.Scanner;
+
+public class desafios {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        String nome;
+        int idade;
+        while (true) { // estrutura de repetição while , recebendo true
+            System.out.printf("Digite seu nome: ");
+            nome = ler.next();
+            if (nome.equals("0")) { //
+                break;
+            }
+            System.out.printf("Digite sua idade: ");
+            idade = ler.nextInt();
+           /* if (idade == 0) { // se idade for estritamente igual a 0 pare o programa
+                break;
+            }*/
+            System.out.print("");
+        }
+        System.out.println("continua aqui ... ");
+    }
+
+}
